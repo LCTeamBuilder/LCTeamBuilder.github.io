7f2ef2ee4cc1620c208fc193fd5e0e180eae1dbf
- <SuenoImposibleLCB@gmail.com>
2024-11-15 21:09:50 +0200

    Fix faction tag rendering in skill descriptions

--- src/Consts/Sinners/Rodion/Identities/ThePrincessOfLaManchaland/ThePrincessOfLaManchalandRodionSkill1.ts
+++ src/Consts/Sinners/Rodion/Identities/ThePrincessOfLaManchaland/ThePrincessOfLaManchalandRodionSkill1.ts
@@ -17,8 +17,8 @@ export class ThePrincessOfLaManchalandRodionSkill1 implements Skill {
     readonly SkillLevel: number = +1;
     readonly SkillDescription: SkillDescriptionPart[] = 
         [new SkillDescriptionPart("[Combat Start] On self and 1 ally with the highest max HP: consume up to 20 [Bloodfeast] and apply 1 [Blooming Thorn] for every 10 [Bloodfeast] consumed<br>"+
-            "- Prioritizes <Bloodfiend> allies<br>"+
-            "- If the affected ally is a <Bloodfiend>, this unit applies 1 additional [Blooming Thorn]<br>"+
+            "- Prioritizes &lt;Bloodfiend&gt; allies<br>"+
+            "- If the affected ally is a &lt;Bloodfiend&gt;, this unit applies 1 additional [Blooming Thorn]<br>"+
             "- If this unit failed to consume [Bloodfeast], gain 5 [Bleed]<br>"+
             "[On Use] Coin Power +1 for every 6 [Bleed] on the target (max 2)"),
         new SkillDescriptionPart("[On Hit] Inflict 1 [Bleed]<br>"+
--- src/Consts/Sinners/Rodion/Identities/ThePrincessOfLaManchaland/ThePrincessOfLaManchalandRodionSkill2.ts
+++ src/Consts/Sinners/Rodion/Identities/ThePrincessOfLaManchaland/ThePrincessOfLaManchalandRodionSkill2.ts
@@ -17,8 +17,8 @@ export class ThePrincessOfLaManchalandRodionSkill2 implements Skill {
     readonly SkillLevel: number = +2;
     readonly SkillDescription: SkillDescriptionPart[] = 
         [new SkillDescriptionPart("[Combat Start] On self and 1 ally with the least max HP: apply 1 [Attack Power Up], consume up to 30 [Bloodfeast] and apply 1 [Blooming Thorn] for every 10 [Bloodfeast] consumed (2 times per turn)<br>"+
-            "- Prioritizes <Bloodfiend> allies<br>"+
-            "- If the affected ally is a <Bloodfiend>, this unit applies additional 1 [Attack Power Up] and 2 [Blooming Thorn]<br>"+
+            "- Prioritizes &lt;Bloodfiend&gt; allies<br>"+
+            "- If the affected ally is a &lt;Bloodfiend&gt;, this unit applies additional 1 [Attack Power Up] and 2 [Blooming Thorn]<br>"+
             "- If this unit failed to consume [Bloodfeast], gain 5 [Bleed]<br>"+
             "[On Use] Coin Power +1 for every 6 [Bleed] on the target (max 2)"),
         new SkillDescriptionPart("[On Hit] Inflict +2 [Rupture] Count<br>"+
